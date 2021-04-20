5d9e14072b95f0f8f5f4b788968ecbb8eb6632ee
Tim Kräuter <tim.kraeuterr@gmail.com>
2021-04-20 09:47:05 +0200

    Changed name and disabled plugin if no debugging is running.

--- src/main/java/DebugPOCAction.java
+++ src/main/java/DebugPOCAction.java
@@ -7,6 +7,7 @@ import com.intellij.xdebugger.XDebugSessionListener;
 import com.intellij.xdebugger.XDebuggerManager;
 import com.intellij.xdebugger.frame.XStackFrame;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 
@@ -14,24 +15,35 @@ public class DebugPOCAction extends AnAction {
 
     @Override
     public void update(AnActionEvent e) {
-        // Using the event, evaluate the context, and enable or disable the action.
-        System.out.println("update");
+        e.getPresentation().setEnabled(isDebuggingInProgress());
+    }
+
+    private boolean isDebuggingInProgress() {
+        return this.getDebugSessionIfExists() != null;
     }
 
     @Override
     public void actionPerformed(@NotNull final AnActionEvent e) {
         System.out.println("test123");
-        // I assume atm there is only one open project.
+        final XDebugSession debugSession = getDebugSessionIfExists();
+
+        if (debugSession != null) {
+            debugSession.addSessionListener(new XDebugSessionListener() {
+                @Override
+                public void sessionPaused() {
+                    System.out.println("Next step in debugger!");
+                    final XStackFrame currentStackFrame = debugSession.getCurrentStackFrame();
+                }
+            });
+        } else {
+            System.out.println("No debugging session active!");
+        }
+    }
+
+    @Nullable
+    private XDebugSession getDebugSessionIfExists() {
+        // Atm, I assume there is only one open project.
         final Project project = Arrays.asList(ProjectManager.getInstance().getOpenProjects()).get(0);
-
-        final XDebugSession debugSession = XDebuggerManager.getInstance(project).getCurrentSession();
-
-        debugSession.addSessionListener(new XDebugSessionListener() {
-            @Override
-            public void sessionPaused() {
-                System.out.println("Next step in debugger!");
-                final XStackFrame currentStackFrame = debugSession.getCurrentStackFrame();
-            }
-        });
+        return XDebuggerManager.getInstance(project).getCurrentSession();
     }
 }
