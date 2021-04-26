ed014da19240ba47f12774fffe489fb323a48c68
Tim Kräuter <tim.kraeuterr@gmail.com>
2021-04-26 13:45:11 +0200

    Added dependency for JavaValue

--- src/main/java/DebugPOCAction.java
+++ src/main/java/DebugPOCAction.java
@@ -1,3 +1,6 @@
+import com.intellij.debugger.engine.JavaValue;
+import com.intellij.debugger.engine.evaluation.EvaluationContextImpl;
+import com.intellij.debugger.ui.impl.watch.FieldDescriptorImpl;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
@@ -45,68 +48,19 @@ public class DebugPOCAction extends AnAction {
                         @Override
                         public void addChildren(@NotNull XValueChildrenList children, boolean last) {
                             for (int i = 0; i < children.size(); i++) {
-                                XNamedValue value = (XNamedValue) children.getValue(i);
+                                JavaValue value = (JavaValue) children.getValue(i);
+                                String typeName = value.getDescriptor().getValue().type().name();// fully qualified name of the type.
                                 value.computeChildren(new XCompositeNode() {
                                     @Override
                                     public void addChildren(@NotNull XValueChildrenList children, boolean last) {
                                         // We need Type, fields and associations
                                         for (int i = 0; i < children.size(); i++) {
-                                            XNamedValue value2 = (XNamedValue) children.getValue(i);
-                                            value2.computePresentation(new XValueNode() {
-                                                @Override
-                                                public void setPresentation(@Nullable final Icon icon, @NonNls @Nullable final String type, @NonNls @NotNull final String value, final boolean hasChildren) {
-                                                    System.out.println("Type:" + type);
-                                                    System.out.println("Value:" + value);
-                                                }
-
-                                                @Override
-                                                public void setPresentation(@Nullable final Icon icon, @NotNull final XValuePresentation presentation, final boolean hasChildren) {
-                                                    System.out.println("Type: " + presentation.getType());
-                                                    presentation.renderValue(new XValueTextRendererBase() {
-                                                        @Override
-                                                        protected void renderRawValue(@NotNull @NlsSafe final String value, @NotNull final TextAttributesKey key) {
-                                                            System.out.println("Rendered value::" + value);
-                                                        }
-
-                                                        @Override
-                                                        public void renderValue(@NotNull @NlsSafe final String value) {
-                                                            System.out.println("Rendered value::" + value);
-                                                        }
-
-                                                        @Override
-                                                        public void renderStringValue(@NotNull @NlsSafe final String value, @Nullable @NlsSafe final String additionalSpecialCharsToHighlight, final int maxLength) {
-                                                            System.out.println("Rendered value::" + value);
-                                                        }
-
-                                                        @Override
-                                                        public void renderComment(@NotNull @NlsSafe final String comment) {
-                                                            System.out.println("Rendered comment::" + comment);
-                                                        }
-
-                                                        @Override
-                                                        public void renderSpecialSymbol(@NotNull @NlsSafe final String symbol) {
-                                                            System.out.println("Rendered symbol::" + symbol);
-                                                        }
-
-                                                        @Override
-                                                        public void renderError(@NotNull @NlsSafe final String error) {
-                                                            System.out.println("Rendered error:" + error);
-                                                        }
-                                                    });
-                                                }
-
-                                                @SuppressWarnings("UnstableApiUsage")
-                                                @Override
-                                                public void setPresentation(@Nullable final Icon icon, @NonNls @Nullable final String type, @NonNls @NotNull final String separator, @NonNls @Nullable final String value, final boolean hasChildren) {
-                                                    System.out.println("Type:" + type);
-                                                    System.out.println("Value:" + value);
-                                                }
-
-                                                @Override
-                                                public void setFullValueEvaluator(@NotNull final XFullValueEvaluator fullValueEvaluator) {
-                                                    System.out.println("Value:" + value);
-                                                }
-                                            }, XValuePlace.TREE);
+                                            JavaValue value2 = (JavaValue) children.getValue(i);
+                                            value2.getDescriptor().isPrimitive();
+                                            if (value2.getDescriptor().isPrimitive()) {
+                                                FieldDescriptorImpl fieldDescriptor = (FieldDescriptorImpl) value2.getDescriptor();
+                                                // fieldDescriptor.calcValue();
+                                            }
                                         }
                                     }
 
