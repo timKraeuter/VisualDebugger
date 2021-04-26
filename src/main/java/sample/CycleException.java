ff594e0ea3bbbb3f71236b14a04b980a5dde1bc7
Tim Kräuter <tim.kraeuterr@gmail.com>
2021-04-26 09:34:28 +0200

    Added partslist as example for testing the debugger.

---
+++ src/main/java/sample/CycleException.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class CycleException extends RuntimeException {
+	
+	public CycleException(final String string) {
+		super(string);
+	}
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2400740596677846068L;
+	
+}
