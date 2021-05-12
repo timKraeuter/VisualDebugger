3dc8d89e7b5155dc34e04245eef2b38ee61b1b54
Tim Kräuter <tim.kraeuterr@gmail.com>
2021-05-12 09:54:13 +0200

    Added ui with sample icon from the java visualizer plugin.

--- src/main/java/no/hvl/tk/visualDebugger/debugging/DebugListener.java
+++ src/main/java/no/hvl/tk/visualDebugger/debugging/DebugListener.java
@@ -1,17 +1,28 @@
 package no.hvl.tk.visualDebugger.debugging;
 
+import com.intellij.execution.ui.RunnerLayoutUi;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.ActionToolbar;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.ui.SimpleToolWindowPanel;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.ui.content.Content;
 import com.intellij.util.concurrency.Semaphore;
+import com.intellij.util.ui.UIUtil;
 import com.intellij.xdebugger.XDebugSession;
 import com.intellij.xdebugger.XDebugSessionListener;
 import com.intellij.xdebugger.frame.XStackFrame;
+import no.hvl.tk.visualDebugger.DebuggVisualizerListener;
 import no.hvl.tk.visualDebugger.debugging.visualization.ConsoleDebuggingVisualizer;
 import no.hvl.tk.visualDebugger.debugging.visualization.DebuggingVisualizer;
 
+import javax.swing.*;
 import java.util.Objects;
 
 public class DebugListener implements XDebugSessionListener {
     private static final Logger LOGGER = Logger.getInstance(DebugListener.class);
+    private static final String CONTENT_ID = "no.hvl.tk.VisualDebugger";
 
     public static final int default_debugging_depth = 10; // TODO increased to 10 for testing
 
@@ -40,6 +51,8 @@ public class DebugListener implements XDebugSessionListener {
         final XStackFrame currentStackFrame = debugSession.getCurrentStackFrame();
         Objects.requireNonNull(currentStackFrame, "Stack frame unexpectedly was null.");
 
+        initUI();
+
         final DebuggingVisualizer debuggingVisualizer = new ConsoleDebuggingVisualizer();
         final CounterBasedLock lock = new CounterBasedLock();
         final NodeDebugVisualizer nodeVisualizer = new NodeDebugVisualizer(
@@ -58,5 +71,31 @@ public class DebugListener implements XDebugSessionListener {
     @Override
     public void stackFrameChanged() {
         System.out.println("Stack frame changed");
+    }
+
+    private void initUI() {
+        final JPanel panel = new JPanel();
+        panel.add(new JLabel("Test", SwingConstants.CENTER));
+
+        final SimpleToolWindowPanel container = new SimpleToolWindowPanel(false, true);
+        final ActionManager actionManager = ActionManager.getInstance();
+        ActionToolbar actionToolbar = actionManager.createActionToolbar(
+                "DebugVisualizer.VisualizerToolbar",
+                (DefaultActionGroup) actionManager.getAction("DebugVisualizer.VisualizerToolbar"),
+                false
+        );
+        actionToolbar.setTargetComponent(panel);
+        container.setToolbar(actionToolbar.getComponent());
+        container.setContent(panel);
+
+        RunnerLayoutUi ui = debugSession.getUI();
+        Content content = ui.createContent(
+                CONTENT_ID,
+                container,
+                "Visual Debugger",
+                IconLoader.getIcon("/icons/viz.png", DebuggVisualizerListener.class),
+                null);
+        content.setCloseable(false);
+        UIUtil.invokeLaterIfNeeded(() -> ui.addContent(content));
     }
 }
--- src/test/java/no/hvl/tk/visualDebugger/partsList/PartsListTest.java
+++ src/test/java/no/hvl/tk/visualDebugger/partsList/PartsListTest.java
@@ -14,6 +14,6 @@ public class PartsListTest {
         folding_wall_table.addPart(Material.create("Wood screw D3,5 x 20mm", 1), 26);
         folding_wall_table.addPart(Material.create("Wood screw D4 x 45mm", 1), 10);
         // Real cost is 71. Forgot the assemblyCost of 5.
-        assertEquals(66, folding_wall_table.getOverallCost());
+        assertEquals(71, folding_wall_table.getOverallCost());
     }
 }
