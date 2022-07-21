d4ee10e907dadffcf96512ec58d70ca851fd7603
Tim Kräuter <tim.kraeuterr@gmail.com>
2022-07-21 16:11:02 +0200

    Given, when, then parts list test.

--- src/test/java/no/hvl/tk/visual/debugger/manueltests/partsList/PartsListTest.java
+++ src/test/java/no/hvl/tk/visual/debugger/manueltests/partsList/PartsListTest.java
@@ -10,19 +10,17 @@ class PartsListTest {
 
     @Test
     void overallCostForFoldingWallTableTest() {
+        // Given
         final Product folding_wall_table = Product.create("Folding wall table", 5);
         folding_wall_table.addPart(Material.create("Main support", 10), 1);
         folding_wall_table.addPart(Material.create("Hinge", 5), 4);
         folding_wall_table.addPart(Material.create("Wood screw D3,5 x 20mm", 1), 26);
         folding_wall_table.addPart(Material.create("Wood screw D4 x 45mm", 1), 10);
 
-        assertEquals(71, folding_wall_table.getOverallCost());
+        // When
+        final int cost = folding_wall_table.getOverallCost();
 
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
+        // Then
+        assertEquals(71, cost);
     }
 }
