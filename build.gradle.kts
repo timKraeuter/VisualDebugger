151fb360de4e526d3772968d15f376187916f553
Tim Kräuter <tim.kraeuterr@gmail.com>
2026-03-29 15:22:43 +0200

    Add manual publish workflow for JetBrains Marketplace

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,14 @@ intellijPlatform {
             untilBuild.set("261.*")
         }
     }
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+    }
 }
 
 tasks.test {
